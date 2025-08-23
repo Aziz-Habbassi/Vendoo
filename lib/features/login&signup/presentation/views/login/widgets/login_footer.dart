517802b9a00c27139a56752225c782992d3a3844
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-08-23 15:29:04 +0100

    Add login with facebook and google section

--- lib/features/login&signup/presentation/views/login/widgets/login_footer.dart
+++ lib/features/login&signup/presentation/views/login/widgets/login_footer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:vendoo/app_routes.dart';
 import 'package:vendoo/core/constants/constants.dart';
@@ -8,28 +9,76 @@ class LoginFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        Text(
-          "Don't have an account ?",
-          style: TextStyle(
-            color: const Color.fromARGB(255, 255, 255, 255),
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        const SizedBox(width: 6),
-        GestureDetector(
-          onTap: () => context.go(AppRoutes.signUp),
-          child: Text(
-            "Sign Up",
-            style: TextStyle(
-              color: kprimaryColor,
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Don't have an account ?",
+              style: TextStyle(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
+            const SizedBox(width: 6),
+            GestureDetector(
+              onTap: () => context.go(AppRoutes.signUp),
+              child: Text(
+                "Sign Up",
+                style: TextStyle(
+                  color: kprimaryColor,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Divider(
+                thickness: 2,
+                color: kprimaryColor,
+                indent: MediaQuery.of(context).size.width * 0.2,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Text(
+              "Or Login using",
+              style: TextStyle(
+                color: kprimaryColor,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Divider(
+                thickness: 2,
+                color: kprimaryColor,
+                endIndent: MediaQuery.of(context).size.width * 0.2,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/images/loginView/facebook_logo.svg",
+              height: 70,
+            ),
+            const SizedBox(width: 50),
+            SvgPicture.asset(
+              "assets/images/loginView/google_logo.svg",
+              height: 60,
+            ),
+          ],
         ),
       ],
     );
