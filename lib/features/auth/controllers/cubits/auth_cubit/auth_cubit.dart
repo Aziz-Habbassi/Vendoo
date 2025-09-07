9750297e9651067fe1410f674378a2bb65a438b8
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-09-07 14:16:32 +0100

    finish sign up cubit

---
+++ lib/features/auth/controllers/cubits/auth_cubit/auth_cubit.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+
+  Future<void> signUpUser({
+    required String email,
+    required String password,
+    required String firstName,
+    required String lastName,
+    required String username,
+  }) async {
+    final supabase = Supabase.instance.client;
+    try {
+      emit(AuthLoading());
+      final authResponse = await supabase.auth.signUp(
+        email: email,
+        password: password,
+      );
+      final user = authResponse.user;
+      if (user != null) {
+        await supabase.from('profiles').insert({
+          'id': user.id,
+          'first_name': firstName,
+          'last_name': lastName,
+          'username': username,
+        });
+      }
+      emit(AuthSuccess());
+    } on AuthException catch (e) {
+      emit(AuthError(errMessage: e.message));
+    } catch (e) {
+      emit(AuthError(errMessage: e.toString()));
+    }
+  }
+}
